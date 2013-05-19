77f7c6d9ac8f8ac737bd22acd30ad97d7e93b7f3
Devin Carr <devin.carr@outlook.com>
2013-05-19 00:32:20 -0500

    Deleted my old Log creation that is no longer useful.

--- FallingBlocks/src/main/java/com/mark/games/fallingblocks/Settings.java
+++ FallingBlocks/src/main/java/com/mark/games/fallingblocks/Settings.java
@@ -16,7 +16,6 @@ public class Settings {
 	public static int[] scoresArray = new int[5];
 	public static boolean showFps;
 	public static boolean playMusic;
-	public static ArrayList<String> logList;
 	
 	public static void load(FileIO files, String file) {
 		BufferedReader in = null;
@@ -33,27 +32,6 @@ public class Settings {
 				scoresArray[i] = Integer.parseInt(in.readLine());
 			
 			in.close();
-		} catch (IOException e) {
-		} catch (NumberFormatException e) {
-		} finally {
-			try {
-				if (in != null){
-					in.close();
-				}
-			} catch (IOException e) {
-			}
-		}
-	}
-	public static void loadLog(FileIO files, String file) {
-		BufferedReader in = null;
-		logList = new ArrayList<String>();
-		try {
-			in = new BufferedReader(new InputStreamReader(
-					files.readFile(file)));
-			String a = "";
-			while ((a = in.readLine()) != null){
-				logList.add(a);
-			}
 		} catch (IOException e) {
 		} catch (NumberFormatException e) {
 		} finally {
@@ -92,6 +70,7 @@ public class Settings {
 			}
 		}
 	}
+
 	public static void addScore(int score) {
         for (int i = 0; i < 5; i++) {
         	Log.d("scores: ", Integer.toString(scoresArray[i]));
@@ -103,43 +82,4 @@ public class Settings {
             }
         }
     }
-	public static void writeLog2(FileIO files, String tag, String message){
-		BufferedWriter out= null;
-
-		try {
-			//create the stream using appendToFile() if you don't want to erase the contents of the file when writing
-			out = new BufferedWriter(new OutputStreamWriter(files.appendToFile(".log")));
-			out.append(tag + ": " + message);
-			out.newLine();
-			out.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (out != null)
-					out.close();
-			} catch (IOException e) {
-
-			}
-		}
-	}
-	public static void deleteLog(FileIO files){
-		BufferedWriter out= null;
-		try {
-			//delete the log file easily
-			out = new BufferedWriter(new OutputStreamWriter(files.writeFile(".log")));
-			out.write("");
-			out.flush();
-			out.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (out != null)
-					out.close();
-			} catch (IOException e) {
-
-			}
-		}
-	}
 }
